4d7d3cdc846cc2e4ee2f9afe077fd0f2f3e52067
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-08-24 18:50:39 +0530

    Add files via upload

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://github.com/TeamAloneOp/AloneX"
+            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
     ]
     return buttons
