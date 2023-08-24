891a5a1a394148a5dbf1184e25a46cdcc94920f2
AlonePvtGithub <142078798+AlonePvtGithub@users.noreply.github.com>
2023-08-24 18:28:48 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.GITHUB_REPO‎),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://github.com/TeamAloneOp/AloneX"
         ],
     ]
     return buttons
