48f1c1441622a53bcbec3fab60444f4d511856fd
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-08-24 18:53:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AlonePvtGithub/AloneXPvtNew",
+    "https://github.com/TeamAloneOp/AloneXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
