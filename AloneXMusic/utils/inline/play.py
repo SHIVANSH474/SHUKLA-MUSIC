79bac891035fdcd478439e8090886d679233015f
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-08-25 19:14:29 +0530

    Update play.py

--- AloneXMusic/utils/inline/play.py
+++ AloneXMusic/utils/inline/play.py
@@ -59,11 +59,36 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        [
+            InlineKeyboardButton(
+                text="▷",
+                callback_data=f"ADMIN Resume|{chat_id}",
+            ),
+            InlineKeyboardButton(
+                text="II", callback_data=f"ADMIN Pause|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="↻", callback_data=f"ADMIN Replay|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="▢", callback_data=f"ADMIN Stop|{chat_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="Sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_CHAT
+            ),
+            InlineKeyboardButton(
+                text="Sᴏᴜʀᴄᴇ", url=config.UPSTREAM_REPO
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="✯ Cʟᴏsᴇ ✯", callback_data=f"close"
+            )
         ],
     ]
     return buttons
@@ -72,12 +97,36 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],   
+            InlineKeyboardButton(
+                text="▷",
+                callback_data=f"ADMIN Resume|{chat_id}",
+            ),
+            InlineKeyboardButton(
+                text="II", callback_data=f"ADMIN Pause|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="↻", callback_data=f"ADMIN Replay|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="▢", callback_data=f"ADMIN Stop|{chat_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="Sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_CHAT
+            ),
+            InlineKeyboardButton(
+                text="Sᴏᴜʀᴄᴇ", url=config.UPSTREAM_REPO
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="✯ Cʟᴏsᴇ ✯", callback_data=f"close"
+            )
+        ],
     ]
     return buttons
 
