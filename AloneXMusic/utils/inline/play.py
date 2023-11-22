bf6d34228fd7f4e9c43ee5f441e25c39f8561d87
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2023-11-23 03:44:10 +0530

    V3 Released

--- AloneXMusic/utils/inline/play.py
+++ AloneXMusic/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "─▷─────────"
+        bar = "▷─────────"
     elif 10 < umm < 20:
-        bar = "──▷────────"
+        bar = "─▷────────"
     elif 20 <= umm < 30:
-        bar = "───▷───────"
+        bar = "──▷───────"
     elif 30 <= umm < 40:
-        bar = "────▷──────"
+        bar = "───▷──────"
     elif 40 <= umm < 50:
-        bar = "─────▷─────"
+        bar = "────▷─────"
     elif 50 <= umm < 60:
-        bar = "──────▷────"
+        bar = "─────▷────"
     elif 60 <= umm < 70:
-        bar = "───────▷───"
+        bar = "──────▷───"
     elif 70 <= umm < 80:
-        bar = "────────▷──"
+        bar = "───────▷──"
     elif 80 <= umm < 95:
-        bar = "─────────▷─"
+        bar = "────────▷─"
     else:
-        bar = "──────────▷"
+        bar = "─────────▷"
     buttons = [
         [
             InlineKeyboardButton(
@@ -61,9 +61,8 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -72,9 +71,8 @@ def stream_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
