9ab3ec768eea5ee9ea92a5b6fb49ecc127ea51ee
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2023-11-23 22:49:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,6 +13,9 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
+# Get Your bot username
+BOT_USERNAME = getenv("BOT_USERNAME" , "AloneXRobot")
+
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
