05f639179b3308a8706220ef2e161875dd0c7494
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2023-11-23 18:04:28 +0530

    Mr Alone

---
+++ AloneXMusic/plugins/tools/deepai.py
@@ -0,0 +1,64 @@
+import requests, config
+from pyrogram import filters
+from AloneXMusic import app
+from pyrogram.enums import ChatAction, ParseMode
+
+api_key ="908fa7e9-220b-4357-ac2f-7eb499005b5f"
+
+
+
+@app.on_message(filters.command(["deep"],  prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
+async def deepchat(app: app, message):
+    name = message.from_user.first_name
+    try:
+        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(f"Hello {name}\nPlease provide text after the /deep command.")
+        else:
+            a = message.text.split(' ', 1)[1]
+
+            data = {
+                'text': a,  
+            }
+
+            headers = {
+                'api-key': api_key,
+            }
+
+            r = requests.post("https://api.deepai.org/api/text-generator", data=data, headers=headers)
+            response = r.json()
+            answer_text = response['output']
+            await message.reply_text(f"{answer_text}")
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ**: {e}")
+
+
+#####
+
+@app.on_message(filters.command(["aby" , ],  prefixes=["b","B"]))
+async def deepchat(app: app, message):
+    name = message.from_user.first_name
+    try:
+        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(f"Hello {name}\nPlease provide text after the /deep command.")
+        else:
+            a = message.text.split(' ', 1)[1]
+
+            data = {
+                'text': a,  
+            }
+
+            headers = {
+                'api-key': api_key,
+            }
+
+            r = requests.post("https://api.deepai.org/api/text-generator", data=data, headers=headers)
+            response = r.json()
+            answer_text = response['output']
+            await message.reply_text(f"{answer_text}")
+    except Exception as e:
+        await message.reply_text(f"ᴇʀʀᴏʀ: {e}")
+
+
+##
