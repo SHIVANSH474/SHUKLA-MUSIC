435b8d1e76bb4d9b4a85b6b1d84d9d8b10a76ce1
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2023-11-23 18:00:15 +0530

    Mr Alone

--- AloneXMusic/plugins/tools/group.py
+++ AloneXMusic/plugins/tools/group.py
@@ -1,10 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from AloneXMusic import app
+from config import OWNER_ID
 # vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
-       await msg.reply("**𝐕ᴏɪᴄᴇ 𝐂ʜᴀᴛ 𝐒ᴛᴀʀᴛᴇᴅ**")
+       await msg.reply("𝐕ᴏɪᴄᴇ 𝐂ʜᴀᴛ 𝐒ᴛᴀʀᴛᴇᴅ")
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
@@ -25,3 +26,56 @@ async def brah3(app :app, message:Message):
              await message.reply(f"{text} 😉")
            except:
              pass
+
+
+####
+
+@app.on_message(filters.command("math", prefixes="/"))
+def calculate_math(client, message):   
+    expression = message.text.split("/math ", 1)[1]
+    try:        
+        result = eval(expression)
+        response = f"ᴛʜᴇ ʀᴇsᴜʟᴛ ɪs : {result}"
+    except:
+        response = "ɪɴᴠᴀʟɪᴅ ᴇxᴘʀᴇssɪᴏɴ"
+    message.reply(response)
+
+###
+@app.on_message(filters.command("leavegroup")& filters.user(OWNER_ID))
+async def bot_leave(_, message):
+    chat_id = message.chat.id
+    text = "𝐒ᴜᴄᴄᴇssғᴜʟʟʏ 𝐋ᴇғᴛ 𝐓ʜᴇ 𝐆ʀᴏᴜᴘ !!."
+    await message.reply_text(text)
+    await app.leave_chat(chat_id=chat_id, delete=True)
+
+
+####
+
+
+@app.on_message(filters.command(["spg"], ["/", "!", "."]))
+async def search(event):
+    msg = await event.respond("Searching...")
+    async with aiohttp.ClientSession() as session:
+        start = 1
+        async with session.get(f"https://content-customsearch.googleapis.com/customsearch/v1?cx=ec8db9e1f9e41e65e&q={event.text.split()[1]}&key=AIzaSyAa8yy0GdcGPHdtD083HiGGx_S0vMPScDM&start={start}", headers={"x-referer": "https://explorer.apis.google.com"}) as r:
+            response = await r.json()
+            result = ""
+            
+            if not response.get("items"):
+                return await msg.edit("No results found!")
+            for item in response["items"]:
+                title = item["title"]
+                link = item["link"]
+                if "/s" in item["link"]:
+                    link = item["link"].replace("/s", "")
+                elif re.search(r'\/\d', item["link"]):
+                    link = re.sub(r'\/\d', "", item["link"])
+                if "?" in link:
+                    link = link.split("?")[0]
+                if link in result:
+                    # remove duplicates
+                    continue
+                result += f"{title}\n{link}\n\n"
+            prev_and_next_btns = [Button.inline("▶️Next▶️", data=f"next {start+10} {event.text.split()[1]}")]
+            await msg.edit(result, link_preview=False, buttons=prev_and_next_btns)
+            await session.close()
