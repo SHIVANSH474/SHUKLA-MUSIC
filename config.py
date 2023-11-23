eff0f324a5a72fe9df29803dbbdc8ec75de0cabd
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2023-11-23 22:58:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get Your bot username
-BOT_USERNAME = getenv("BOT_USERNAME" , "AloneXRobot")
+BOT_USERNAME = getenv("BOT_USERNAME")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
