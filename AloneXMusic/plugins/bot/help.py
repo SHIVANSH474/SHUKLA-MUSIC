c8f0d82d7ddce047b7fff995f66d4d3a46028734
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2023-11-27 02:38:56 +0530

    Update help.py

--- AloneXMusic/plugins/bot/help.py
+++ AloneXMusic/plugins/bot/help.py
@@ -39,7 +39,7 @@ async def helper_private(
         _ = get_string(language)
         keyboard = help_pannel(_)
         await update.reply_photo(
-            photo=START_IMG_URL,
+            photo=random.choice(START_IMG_URL),
             caption=_["help_1"].format(SUPPORT_CHAT),
             reply_markup=keyboard,
         )
