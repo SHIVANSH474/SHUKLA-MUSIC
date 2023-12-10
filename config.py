7ebdc087f3a744d055518a0511f0d060d00ec14b
Imshukla87 <149559899+Imshukla87@users.noreply.github.com>
2023-12-10 22:40:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/ABOUT_SHIVANSHOP",
+    "https://github.com/itzshukla/STRANGER-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
