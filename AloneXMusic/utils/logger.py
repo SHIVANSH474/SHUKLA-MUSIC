bff27eca9ae9e173ac8335e04d1b31864f3d3617
Imshukla87 <149559899+Imshukla87@users.noreply.github.com>
2023-12-12 10:32:26 +0530

    Update logger.py

--- AloneXMusic/utils/logger.py
+++ AloneXMusic/utils/logger.py
@@ -8,7 +8,7 @@ from config import LOGGER_ID
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} ᴀʟᴏɴᴇ ᴘʟᴀʏ ʟᴏɢ</b>
+<b>{app.mention} ᴍᴇʟᴀɴɪᴀ ᴘʟᴀʏ ʟᴏɢ</b>
 
 <b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
 <b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
