794998733a945c7fe76baae2d22abe03fd93831e
SHIVANSH XD <149559899+SHIVANSH474@users.noreply.github.com>
2024-02-02 13:40:07 +0530

    Update start.py

--- AloneXMusic/utils/inline/start.py
+++ AloneXMusic/utils/inline/start.py
@@ -31,7 +31,6 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
     ]
     return buttons
