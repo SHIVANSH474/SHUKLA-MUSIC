c152555aa4943a8e892b11ad1586effc2663e37d
SHIVANSH XD <149559899+SHIVANSH474@users.noreply.github.com>
2024-02-05 10:46:56 +0530

    Update help.py

--- AloneXMusic/plugins/bot/help.py
+++ AloneXMusic/plugins/bot/help.py
@@ -12,7 +12,7 @@ from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
 
-@app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
+@app.on_message(filters.command(["mhelp"]) & filters.private & ~BANNED_USERS)
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
 async def helper_private(
     client: app, update: Union[types.Message, types.CallbackQuery]
@@ -45,7 +45,7 @@ async def helper_private(
         )
 
 
-@app.on_message(filters.command(["help"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["mhelp"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
