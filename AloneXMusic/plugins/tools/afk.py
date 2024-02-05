1449e1ce746722cb459204322733c2820439bf01
SHIVANSH XD <149559899+SHIVANSH474@users.noreply.github.com>
2024-02-05 10:55:06 +0530

    Update afk.py

--- AloneXMusic/plugins/tools/afk.py
+++ AloneXMusic/plugins/tools/afk.py
@@ -9,7 +9,7 @@ from AloneXMusic.mongo.afkdb import add_afk, is_afk, remove_afk
 
 
 
-@app.on_message(filters.command(["afk", "ye"], prefixes=["/", "b"]))
+@app.on_message(filters.command(["afk", "ye"], prefixes=["/", "b", "B"]))
 async def active_afk(_, message: Message):
     if message.sender_chat:
         return
