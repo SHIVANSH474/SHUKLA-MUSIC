932c93d630af248038c8730907199926056fd1fb
SHIVANSH XD <149559899+SHIVANSH474@users.noreply.github.com>
2024-02-05 10:44:33 +0530

    Update ping.py

--- AloneXMusic/plugins/tools/ping.py
+++ AloneXMusic/plugins/tools/ping.py
@@ -12,7 +12,7 @@ from AloneXMusic.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 
 
-@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["mping", "malive"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
